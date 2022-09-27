2329a7415b7f8ea1dd27888e7eff105f2e5eacd5
Pratham Shroff <pshroff@bu.edu>
2022-09-27 19:10:29 -0400

    Sliders almost working

--- app/src/main/java/com/example/temperatureconverter/MainActivity.java
+++ app/src/main/java/com/example/temperatureconverter/MainActivity.java
@@ -3,14 +3,8 @@ package com.example.temperatureconverter;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
-import android.view.View;
-import android.widget.Button;
-import android.widget.EditText;
-import android.widget.RadioButton;
-import android.widget.RadioGroup;
 import android.widget.SeekBar;
 import android.widget.TextView;
-import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -21,6 +15,8 @@ public class MainActivity extends AppCompatActivity {
     private TextView msg;
     private SeekBar celsius_seekbar;
     private SeekBar fahr_seekbar;
+    private Integer cel_set;
+    private Integer fah_set;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -37,6 +33,62 @@ public class MainActivity extends AppCompatActivity {
 
         celsius_seekbar = (SeekBar) findViewById(R.id.celsius_seekbar);
         fahr_seekbar = (SeekBar) findViewById(R.id.fahr_seekBar);
+
+        celsius_seekbar.setOnSeekBarChangeListener(new SeekBar.OnSeekBarChangeListener() {
+            @Override
+            public void onProgressChanged(SeekBar seekBar, int i, boolean b) {
+                celsius_val.setText(String.valueOf(celsius_seekbar.getProgress()));
+
+                if (Integer.parseInt(String.valueOf(celsius_seekbar.getProgress())) <= 20){
+                    msg.setText("I wish it were warmer.");
+                }
+                else{
+                    msg.setText("I wish it were colder.");
+                }
+            }
+
+            @Override
+            public void onStartTrackingTouch(SeekBar seekBar) {
+
+            }
+
+            @Override
+            public void onStopTrackingTouch(SeekBar seekBar) {
+                celsius_val.setText(String.valueOf(celsius_seekbar.getProgress()));
+
+                fah_set = (Integer.parseInt(String.valueOf(celsius_seekbar.getProgress()))) * (9 / 5) + 32;
+
+                fahr_seekbar.setProgress(Integer.parseInt(String.valueOf(fah_set)));
+            }
+        });
+
+        fahr_seekbar.setOnSeekBarChangeListener(new SeekBar.OnSeekBarChangeListener() {
+            @Override
+            public void onProgressChanged(SeekBar seekBar, int i, boolean b) {
+                fahr_val.setText(String.valueOf(fahr_seekbar.getProgress()));
+            }
+
+            @Override
+            public void onStartTrackingTouch(SeekBar seekBar) {
+
+            }
+
+            @Override
+            public void onStopTrackingTouch(SeekBar seekBar) {
+
+                if (Integer.parseInt(String.valueOf(fahr_seekbar.getProgress())) < 32){
+                    fahr_seekbar.setProgress(32);
+                }
+
+                fahr_val.setText(String.valueOf(fahr_seekbar.getProgress()));
+
+                cel_set = (Integer.parseInt(String.valueOf(fahr_seekbar.getProgress())) - 32) * (5 / 9);
+
+                celsius_seekbar.setProgress(Integer.parseInt(String.valueOf(cel_set)));
+
+            }
+        });
+
 
     }
 }
